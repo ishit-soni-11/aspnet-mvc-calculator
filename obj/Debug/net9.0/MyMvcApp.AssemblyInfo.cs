8f7a5f44247dcacddca10fc3702436a03dcf5b57
ishit-soni-11 <ishitsoni1418@gmail.com>
2025-07-07 16:09:40 -0400

    Stable Version Release

--- obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a99b41014edc98686db1883ba829eae7ecafbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36b7a869686c7a338d6499aecef6a7f7237ea1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
